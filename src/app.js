b1a0f1181306d91ea9bda8ec98c81c52933b4291
JesusCambron <manuel.cam98@gmail.com>
2023-03-18 05:24:03 +0000

    Uncomment cors import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express, { json } from 'express';
 import { SERVER_PORT } from './config/index.js';
 import router from './routes/index.js';
-// import cors from 'cors';
+import cors from 'cors';
 
 const PORT = SERVER_PORT || 3001;
 
