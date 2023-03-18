c85b5371c20a665cb763d3ee52bb56ef83a76676
JesusCambron <manuel.cam98@gmail.com>
2023-03-18 03:21:22 +0000

    Delete cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express, { json } from 'express';
 import { SERVER_PORT } from './config/index.js';
 import router from './routes/index.js';
-import cors from 'cors';
+// import cors from 'cors';
 
-const PORT = SERVER_PORT || 30001;
+const PORT = SERVER_PORT || 3001;
 
 const app = express();
 
 app.use(json());
-app.use(cors())
+// app.use(cors());
 
 /* ROUTES */
-app.use('/api', router)
+app.use('/api', router);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}...`));
