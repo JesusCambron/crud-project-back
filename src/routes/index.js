99f3351caaebdc1def2ef58978ce3a6d0deaabd1
JesusCambron <manuel.cam98@gmail.com>
2023-03-18 04:17:21 +0000

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,25 +2,27 @@ import Router from "express";
 import { readdirSync } from "fs";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { router as clientRouter } from "./clients.js"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const PATH_ROUTER = `${__dirname}`;
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const PATH_ROUTER = `${__dirname}`;
 const router = Router();
 
-const cleanFileName = (fileName) => {
-  const file = fileName.split('.').shift();
-  return file;
-}
+// const cleanFileName = (fileName) => {
+//   const file = fileName.split('.').shift();
+//   return file;
+// }
 
-readdirSync(PATH_ROUTER).filter((fileName) => {
-  const cleanName = cleanFileName(fileName);
-  if (cleanName !== 'index') {
-    console.log(`Loading route... /${cleanName}`);
-    import(`./${cleanName}.js`).then((moduleRouter) => {
-      router.use(`/${cleanName}`, moduleRouter.router);
-    })
-  }
-})
+// readdirSync(PATH_ROUTER).filter((fileName) => {
+//   const cleanName = cleanFileName(fileName);
+//   if (cleanName !== 'index') {
+//     console.log(`Loading route... /${cleanName}`);
+//     import(`./${cleanName}.js`).then((moduleRouter) => {
+//       router.use(`/${cleanName}`, moduleRouter.router);
+//     })
+//   }
+// })
+router.use(`/clients`, clientRouter);
 
 export default router;
